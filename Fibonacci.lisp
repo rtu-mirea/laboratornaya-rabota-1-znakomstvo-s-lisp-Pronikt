0614fe18cddb2b0d1e0332e28a683b873bb16e54
Alexandr <53902219+Pronikt@users.noreply.github.com>
2019-10-05 10:32:47 +0300

    Update Fibonacci.lisp

--- Fibonacci.lisp
+++ Fibonacci.lisp
@@ -1,3 +1,3 @@
 (defun fib_ (n lst) (cond ((= n 1) lst)
                           ((= n 2) lst)
-                          (t (fib_ (sub1 n) (cons (+ (car lst) (cadr lst)) lst)))))
+                          (t (fib_ (- n 1) (cons (+ (car lst) (cadr lst)) lst)))))
